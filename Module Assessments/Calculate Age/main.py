b2823fed29f7c0a40b86d0d18b2a38f766d4b77c
ahmed essam <ahmed.essam@meti.dev>
2024-12-30 18:20:26 +0200

    calc_age validate

--- Module Assessments/Calculate Age/main.py
+++ Module Assessments/Calculate Age/main.py
@@ -1,7 +1,13 @@
 def main():
     start_year = input("Enter the start year: ")
     end_year = input("Enter the End year: ")
-    age = int(end_year) - int(start_year)
-    print("Age of the person is ", age, " years")
+    if (start_year.isnumeric() and end_year.isnumeric()):
+        if(end_year >= start_year):    
+            age = int(end_year) - int(start_year)
+            print("Age of the person is ", age, " years")
+        else:
+            print("Input logical error!")
+    else:
+        print("Invalid Input!!")
 
 main()
