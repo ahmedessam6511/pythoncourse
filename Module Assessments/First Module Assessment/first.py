90d0941e34d9dd17d4a31d3c3bb7068c37256e85
ahmed essam <ahmed.essam@meti.dev>
2024-12-30 16:08:18 +0200

    First Modeule Assessment

---
+++ Module Assessments/First Module Assessment/first.py
@@ -0,0 +1,7 @@
+"""
+write a program that prints an introduction to you
+ (include your name, age, and 
+ a summary of what you want to learn in this project
+"""
+
+print("Name: Ahmed Essam\nAge: 26\nI want to learn python for ML and Data science")
