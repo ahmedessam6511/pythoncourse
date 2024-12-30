5cbfbd06656b4251cfa340cf3e057f0ce0bcc011
ahmed essam <ahmed.essam@meti.dev>
2024-12-30 18:14:04 +0200

    update calc_age

--- Module Assessments/Calculate Age/main.py
+++ Module Assessments/Calculate Age/main.py
@@ -2,6 +2,6 @@ def main():
     start_year = input("Enter the start year: ")
     end_year = input("Enter the End year: ")
     age = int(end_year) - int(start_year)
-    print("Your age is >>", age)
+    print("Age of the person is ", age, " years")
 
 main()
