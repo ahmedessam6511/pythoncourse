6c848690ddb896317b9281bc0dcfc4b22d18feab
ahmed essam <ahmed.essam@meti.dev>
2024-12-30 19:42:13 +0200

    circle

---
+++ Module Assessments/Calculate parameters of the Circle/main.py
@@ -0,0 +1,5 @@
+import math
+radius = float(input("Please enter the radius of the circle: "))
+area = math.pi * (radius**2)
+circumference = 2 * math.pi * radius
+print(f"Area equal {area} \n Circumference equal {circumference}")
