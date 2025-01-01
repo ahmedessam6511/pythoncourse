f17a3ea14f1e46b4960379655af6d872a0139cd5
ahmed essam <ahmed.essam@meti.dev>
2025-01-01 18:55:12 +0200

    merge sort

--- Module Assessments/Draft/main.py
+++ Module Assessments/Draft/main.py
@@ -1,11 +1,8 @@
-def main():
-    ls=[10, 2, 7,23, 5,11,15]
-    ## Type your code here
-    dict_out= dict()
-    dict_out["min"] = min(ls)
-    dict_out["max"] = max(ls)
-    ###########
-    print(dict_out)
-    return dict_out
- 
-main()
+
+
+
+if __name__== "__main__":
+    
+    arr = list([9389,24,424])
+
+    print(arr)
