3fc4712a3e82bf52f113b3ef87863fc32b3fe546
ahmed essam <ahmed.essam@meti.dev>
2025-01-14 20:08:32 +0200

    Module 5

--- JS/main.js
+++ JS/main.js
@@ -1,11 +1,37 @@
-var x = 9890;
-var y = "hello";
-var isValide = false;
-var date = new Date();
+let title_val = "Elzero", decrip_val = "Elzero Web School", date_val = "25/10";
+let cardVal = `
+    <div>
+        <h3> Title: ${title_val}</h3>
+        <p>Description: ${decrip_val}</p>
+        <span>Date: ${date_val}</span>
+    </div>
+`;
 
-var name;
+document.write(cardVal);
+document.write(cardVal);
+document.write(cardVal);
 
-name = "ahmed Essam";
+console.log(212 - "kjhgjk");
 
-console.log(x)
+console.log(-"23");
+console.log(+0xff);
+console.log(+NaN);
+console.log(+null);
+console.log(+false);
+console.log(+true);
+console.log(-true);
+console.log(-"-787");
+console.log("50"+10);
+
+let a = 10;
+let b = "20";
+let c = 80;
+
+console.log(++a + +b++ + +c++ - a);
+
+
+
+
+
+
 
